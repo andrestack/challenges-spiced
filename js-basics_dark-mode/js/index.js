8c2c224c2c0f3777a8839cef3ba2a2a8f4ad2615
André Silva <andre.silva@posteo.de>
2023-02-22 10:50:31 +0100

    Add const variables

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,7 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
