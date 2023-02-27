f298ede79264b959531008555abeb9925db22861
André Silva <andre.silva@posteo.de>
2023-02-27 12:32:42 +0100

    add final pizza

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -8,28 +8,81 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
+/* Cleaner alternative
+ 
+function updatedGain() {
+let pizzaSize1 = +pizzaInput1.value
+ let pizzaSize2 = +pizzaInput2.value
+ calculatePizzaGain(pizzaSize1,pizzaSize2)
+
+}
+
+Now in both eventlisteners you could change the paramenters:
+
+pizzaInput1.addEventListener("input", updatedGain);
+
+or
+
 pizzaInput1.addEventListener("input", () => {
+  updatedGain()
+
+}
+
+
+ */
+
+
+pizzaInput1.addEventListener("input", () => {
+ let pizzaSize1 = +pizzaInput1.value
+ let pizzaSize2 = +pizzaInput2.value
+ calculatePizzaGain(pizzaSize1,pizzaSize2)
+ updatePizzaDisplay(pizza1, pizzaInput1.value)
+ updateOutputColor(pizzaSize1,pizzaSize2)
+ 
+
+ 
+  
   // write your code here
 });
 
 pizzaInput2.addEventListener("input", () => {
+  let pizzaSize1 = +pizzaInput1.value
+  let pizzaSize2 = +pizzaInput2.value
+  calculatePizzaGain(pizzaSize1,pizzaSize2)
+  updatePizzaDisplay(pizza2, pizzaInput2.value)
+  updateOutputColor(pizzaSize1,pizzaSize2)
   // write your code here
-});
+})
 
 // Task 1
 
 function calculatePizzaGain(diameter1, diameter2) {
-  // write your code here
+  const area1 = Math.PI * Math.pow(diameter1 / 2, 2)
+  const area2 = Math.PI * Math.pow(diameter2 / 2, 2)
+  const gain = ((area2 - area1) /area1*100);
+  output.textContent = Math.round(gain)
+
+
 }
 
 // Task 2
 
 function updatePizzaDisplay(pizzaElement, newSize) {
+  let newDisplay = newSize / 24 * 100; 
+  pizzaElement.style.width = +newDisplay + "px";
+
+  
   // write your code here
 }
 
 // Task 3
 
 function updateOutputColor(size1, size2) {
+if (size1>size2){
+  outputSection.style.backgroundColor="var(--red)"
+} else {
+  outputSection.style.backgroundColor="var(--green)"
+}
+
   // write your code here
 }
