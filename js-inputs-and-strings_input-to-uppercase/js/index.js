2ca62d44b8d6a37854be04950ef065b0cf349749
André Silva <andre.silva@posteo.de>
2023-02-27 16:57:26 +0100

    Add uppercase challenge

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,5 +1,23 @@
 console.clear();
 
+const inputElement = document.querySelector('[data-js="first-input"]');
+const buttonElement = document.querySelector('[data-js="button-uppercase"]');
+
+
+
+buttonElement.addEventListener("click", () => {
+// event.preventDefault();
+
+const input = inputElement.value; // grabbing the value of the element//
+const upperCase = input.toUpperCase(); // transforming the value //
+inputElement.value=upperCase; //redefining the value with the desired output value//
+
+
+
+
+
+})
+
 /*
 Convert the input value to uppercase when clicking the button
 
