769674e589560f5d0014d09447e74b9779777029
André Silva <andre.silva@posteo.de>
2023-03-01 11:05:18 +0100

    objects

--- js-functions-2_arrow-functions/js/index.js
+++ js-functions-2_arrow-functions/js/index.js
@@ -5,19 +5,32 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
-  const now = new Date();
-  const currentHour = now.getHours();
-  if (currentHour === 0) {
-    return "12am";
-  } else if (currentHour === 12) {
-    return "12pm";
-  } else if (currentHour <= 11) {
-    return currentHour + "am";
-  } else {
-    return currentHour - 12 + "pm";
-  }
-}
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+  
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+const now = (new Date().getHours) => {
+  if (now === 0 && now <= 11){
+    return "Yes!";
+}}
+
+// const time = new Date().getHours();
+// const day = new Date().getDay();
+
+
+
+
 
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
