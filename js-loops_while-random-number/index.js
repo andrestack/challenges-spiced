b041fa73af4e4ed559af7f1e2050a2060b7c8c45
André Silva <andre.silva@posteo.de>
2023-03-05 21:56:29 +0100

    final commit while loop

--- js-loops_while-random-number/index.js
+++ js-loops_while-random-number/index.js
@@ -2,11 +2,16 @@ console.clear();
 
 // `while` loop
 
-let number = 0;
+let number = Math.random();
 let count = 0;
 
 // --v-- write/change code here --v--
+while (number <= 0.9){
+console.log(number);
+number++;
+count++;
 
+}
 // --^-- write/change code here --^--
 
 console.log(
