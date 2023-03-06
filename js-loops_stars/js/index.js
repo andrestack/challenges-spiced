daba624f23333e77cc1cab47824f8a3a3b375517
André Silva <andre.silva@posteo.de>
2023-03-06 10:39:36 +0100

    Add changes to starts

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,13 +2,27 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i < 6; i++){
+    let image = document.createElement("img");
+    // image.setAttribute("src", "./assets/star-empty.svg");
+    starContainer.append(image);
+    
+    
+    if (filledStars === i[i]){
+      image.setAttribute("src", "./assets/star-empty.svg");
+      
+    } else {
+      
+      image.setAttribute("src","./assets/star-filled.svg");
+    }
 
+   }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(1);
