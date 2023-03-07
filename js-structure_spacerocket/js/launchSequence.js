cca2b4e44c402a6f5e5f02b443aeeffd6df4d9d0
André Silva <andre.silva@posteo.de>
2023-03-07 17:36:34 +0100

    Add rocket launch

--- js-structure_spacerocket/js/launchSequence.js
+++ js-structure_spacerocket/js/launchSequence.js
@@ -1,5 +1,16 @@
 // Implement the launch sequence function here and export it as the default export.
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
 import { loadPayload } from "./core/load.js";
-import { FISHSAT, NFSAT } from "./payload/satellites.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
 
-loadPayload(NFSAT, FISHSAT);
+export default function launchSequence() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+  [1, 2, 3, 4, 5].forEach(() => countdown());
+  liftoff();
+  deployPayload();
+}
