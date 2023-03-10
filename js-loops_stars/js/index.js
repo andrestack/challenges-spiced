e88e7cb87bf92eb853124dbdb0ea856e3061ae46
André Silva <andre.silva@posteo.de>
2023-03-10 10:40:26 +0100

    add pull request

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -7,19 +7,22 @@ function renderStars(filledStars) {
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-  starContainer.addEventListener("click", () => renderStars(5));
 
   for (let i = 1; i < 6; i++) {
+    if (i >= filledStars) {
+      // image.setAttribute("src", "./assets/star-filled.svg");
+      image.setAttribute("src", "./assets/star-empty.svg");
+    } else {
+      // image.setAttribute("src", "./assets/star-empty.svg");
+      image.setAttribute("src", "./assets/star-filled.svg");
+    }
+    starContainer.addEventListener("click", () => {
+      renderStars(i);
+    });
     let image = document.createElement("img");
     starContainer.append(image);
-
-    if (i > filledStars) {
-      image.setAttribute("src", "./assets/star-filled.svg");
-      // image.setAttribute("src", "./assets/star-empty.svg");
-    } else {
-      image.setAttribute("src", "./assets/star-empty.svg");
-      //   image.setAttribute("src", "./assets/star-filled.svg");
-    }
   }
   //--^-- your code here --^--
 }
+
+renderStars(0);
