095d28073cfc2cdf8ebdf68c482db1ef5b95aa97
André Silva <andre.silva@posteo.de>
2023-03-10 12:27:32 +0100

    Add to main

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -3,15 +3,14 @@ console.clear();
 const url = "https://swapi.dev/api/people";
 
 async function fetchData() {
-try {
-
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const characters = data.results;
+    console.log(characters[2].eye_color);
+  } catch (error) {
+    console.error("ALARM");
+  }
 }
-
-
-}
-
-
-
-
 
 fetchData();
