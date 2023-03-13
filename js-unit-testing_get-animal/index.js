a56caf9ce09be102e677062a46f5f1a4d939410f
André Silva <andre.silva@posteo.de>
2023-03-13 15:29:11 +0100

    Add animals minus last

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+
+    if (animal === "cats"){
+        return "I totally love cats!"
+    } else if (animal = `${animal}s`){
+        return "I like dogs!"
+    } else {
+        return "I do not like animals at all!"
+    }
+}
