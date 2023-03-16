c14805e78e4c47e6abc9e1052767389319344fed
André Silva <andre.silva@posteo.de>
2023-03-16 15:06:07 +0100

    Create Header component

---
+++ react-nesting_navigation/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import avatar from "./img/avatar.jpg";
+import logo from "./img/logo.jpg";
+import { Header } from "./Components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>content goes here…</main>
+    </>
+  );
+}
