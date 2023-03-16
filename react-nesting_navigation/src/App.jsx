91443d243701616c531f44a15049442735b3ae68
André Silva <andre.silva@posteo.de>
2023-03-16 15:32:16 +0100

    Add logo component

--- react-nesting_navigation/src/App.jsx
+++ react-nesting_navigation/src/App.jsx
@@ -3,6 +3,7 @@ import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
 import { Header } from "./Components/Header";
 import { Navigation } from "./Components/Navigation";
+import { Image } from "./Components/Image";
 
 export default function App() {
   return (
---
+++ react-nesting_navigation/src/Components/Logo.jsx
@@ -0,0 +1,13 @@
+import { Image } from "./Image"
+
+
+export function Logo (){
+    return (
+    <>
+    <Image/>
+        <a href="#">
+        <img className="round-image" src="src/img/logo.jpg" alt="logo" />
+      </a>
+      </>
+    )
+}
