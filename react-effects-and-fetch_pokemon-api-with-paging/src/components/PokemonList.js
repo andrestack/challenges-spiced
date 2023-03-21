0c41518234e50054726ab4206f4996ef853cd087
André Silva <andre.silva@posteo.de>
2023-03-21 12:30:46 +0100

    Add pokemon API

--- react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,29 +2,40 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [next, setNext] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0"
+  );
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(next);
         const data = await response.json();
-        setPokemon(data.results);
+        setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [next]);
+  console.log(pokemon);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      {pokemon.previous && (
+      <button
+        type="button"
+        onClick={() => (pokemon.previous ? setNext(pokemon.previous) : null)}
+      >
+        Previous Page
+      </button>
+      )}
+      <button type="button" onClick={() => setNext(pokemon.next)}>
+        Next Page
+      </button>
       <ul>
-        {pokemon.map(({ name }) => (
+        {pokemon.results?.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
