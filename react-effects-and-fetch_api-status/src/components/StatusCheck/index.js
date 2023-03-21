5a21ea560186dd8324b08ee80e3f1dfd39ce8558
André Silva <andre.silva@posteo.de>
2023-03-21 17:50:34 +0100

    Finish challenge except bonus

--- react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
+++ react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -1,38 +1,50 @@
 import "./StatusCheck.css";
+import { useState } from "react";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
+  let statusIcon = "⁉️";
   // Something needs to change here…
   // ↙️
+
+  /**
+   * Hint 1:
+   * Use the `fetch()` function and pass the `apiStatusUrl` into it
+   *
+   * Hint 2:
+   * The fetch function returns a promise which resolves to a Response
+   * object once it is ready.
+   *
+   * Hint 3:
+   * The Response object has a `ok` property which is true if the response
+   * is okay and false if it is not.
+   **/
+  // --v-- write your code here --v--
+
+  const [correct, setCorrect] = useState(statusIcon);
+
   async function handleCheckApiStatus() {
-    const response = await fetch(apiStatusUrl);
-    const data = await response.json();
-    console.log("show", data);
-    
-    
-    /**
-     * Hint 1:
-     * Use the `fetch()` function and pass the `apiStatusUrl` into it
-     *
-     * Hint 2:
-     * The fetch function returns a promise which resolves to a Response
-     * object once it is ready.
-     *
-     * Hint 3:
-     * The Response object has a `ok` property which is true if the response
-     * is okay and false if it is not.
-     **/
-    // --v-- write your code here --v--
-    // --^-- write your code here --^--
+    try {
+      const response = await fetch(apiStatusUrl);
+      const data = await response.json();
+      setCorrect(data);
+      console.log("hi", data);
+    } catch (error) {
+      {
+        console.error("ALARM");
+      }
+    }
   }
+  // --^-- write your code here --^--
 
   return (
     <article className="status-check">
       <div className="status-check__wrapper">
         <h2 className="status-check__heading">Status:</h2>
-        <span className="status-check__icon">{statusIcon}</span>
+        <span className="status-check__icon">
+          {correct.status === "Ok" ? "✅" : "❌"}
+        </span>
       </div>
       <button
         type="button"
