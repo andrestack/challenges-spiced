47d3a5d83e3d66ca291b19da8858fe87b3fcc32b
André Silva <andre.silva@posteo.de>
2023-04-03 22:06:57 +0200

    Final commit

--- backend-api-routes_products/pages/products/[id].js
+++ backend-api-routes_products/pages/products/[id].js
@@ -1,12 +1,39 @@
 import { useRouter } from "next/router";
-import useSWR  from "swr";
+import useSWR from "swr";
+import styled from "styled-components";
 
-export default function ProductListHandle(request, response) {
-  const { id } = request.query;
-  const { data, error, isLoading } = useSWR(`/api/products/${id}`);
 
-  return 
-  <>
+const StyledDiv=styled.div`
+padding: 10px;
+border: 10px dotted lightblue;
+margin: 10px
+`
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
+export default function ProductListHandle() {
+  const router = useRouter();
+  const {id} = router.query;
+
   
-  </>
-}
+  
+
+  const { data, error, isLoading } = useSWR(`/api/products/${id}`, fetcher);
+  
+
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <StyledDiv>
+      <p>{data.id}</p>
+      <h3>{data.name}</h3>
+      <li>{data.description}</li>
+      <li>
+        {data.price} {data.currency}
+      </li>
+      <li>{data.category}</li>
+      </StyledDiv>
+  
+)}
