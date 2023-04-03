f1f14aae3fd4cba6d5d8d0e5e70f2a49d68ff899
André Silva <andre.silva@posteo.de>
2023-04-03 11:16:57 +0200

    Add first server

--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,3 +1,8 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) =>{
+    response.statusCode = 200;
+    response.end("Hello, Andre!");
+} )
+
+
