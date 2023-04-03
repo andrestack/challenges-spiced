095a9627b780cb95e430dcebf80a31131bb16155
André Silva <andre.silva@posteo.de>
2023-04-03 16:16:40 +0200

    Initial commit

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -2,11 +2,13 @@ import { createServer } from "node:http";
 import Chance from "chance";
 
 const chance = new Chance();
-const age = chance.age()
-const name = chance.name()
-const profession = chance.profession()
+const age = chance.age();
+const name = chance.name({ nationality: "it" });
+const profession = chance.profession();
 
 export const server = createServer((request, response) => {
   response.statusCode = 200;
-  response.end(`Hello, my name is ${name} and I am ${age} years old. I am a ${profession}!`);
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}!`
+  );
 });
