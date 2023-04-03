181a58925cf7964a0ed076558b9a27995bef9ae8
André Silva <andre.silva@posteo.de>
2023-04-03 11:52:28 +0200

    Add second challenge

---
+++ backend-basics_routes/server.js
@@ -0,0 +1,17 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/api/fish/1":
+      response.statusCode = 200;
+      response.end("Schrimp");
+      break;
+    case "/api/fish/2":
+      response.statusCode = 200;
+      response.end("Anemonefish");
+      break;
+    default:
+      response.statusCode = 404;
+      response.end("Not Found!");
+  }
+});
