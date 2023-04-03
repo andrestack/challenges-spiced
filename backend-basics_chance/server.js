6ee55c0d921bc7e10ce4b194692064630c0b1d08
André Silva <andre.silva@posteo.de>
2023-04-03 12:12:50 +0200

    Add chance challenge

---
+++ backend-basics_chance/server.js
@@ -0,0 +1,12 @@
+import { createServer } from "node:http";
+import Chance from "chance";
+
+const chance = new Chance();
+const age = chance.age()
+const name = chance.name()
+const profession = chance.profession()
+
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.end(`Hello, my name is ${name} and I am ${age} years old. I am a ${profession}!`);
+});
