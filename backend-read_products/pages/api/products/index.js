b97a628a1ce3f27d05d10ac0f2d387acb5d929b9
André Silva <andre.silva@posteo.de>
2023-04-12 11:32:12 +0200

    Add API POST request

--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -3,6 +3,18 @@ import Product from "db/models/Product";
 
 export default async function handler(request, response) {
   await dbConnect();
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      const product = new Product(productData);
+      await product.save();
+
+      response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      console.log(error);
+      response.status(400), json({ error: error.message });
+    }
+  }
 
   if (request.method === "GET") {
     const product = await Product.find();
@@ -11,3 +23,26 @@ export default async function handler(request, response) {
     return response.status(405).json({ message: "Method not allowed" });
   }
 }
+/*
+
+switch (request.method === "POST") {
+case:
+try {
+  const productData= request.body;
+  const product = new Product(productData);
+  await product.save()
+
+  response.status(201).json({status: "Product created."})
+} catch (error) {
+  console.log(error);
+  response.status(400),json({error: error.message})
+  
+}
+break
+case (request.method === "GET"):
+const product = await Product.find();
+    return response.status(200).json(product);
+default:
+  response.status(405).json({ message: "Method not allowed" });
+
+} */
